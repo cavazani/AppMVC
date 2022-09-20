3027613b999c4cfb50e119bd74a9b63947e48ee8
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-09-19 22:27:34 -0400

    Ajustes de configurações

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,11 +4,11 @@ using System.Diagnostics;
 
 namespace AppMVC.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger) {
+        //    _logger = logger;
+        //}
 
         public IActionResult Index() {
             return View();
